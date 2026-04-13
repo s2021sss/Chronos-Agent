4c7fc4de42ecaf203ef92f58e2058dd4e04db6d8
Sergei <sergei.mugaiskih@gmail.com>
2026-04-14 00:12:28 +0300

    chore: fix ruff issues

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,8 @@
 from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from alembic import context
 from chronos_agent.db.models import Base  # noqa: F401
 
 config = context.config
